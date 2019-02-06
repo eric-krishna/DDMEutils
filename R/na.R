4fe98e6b70e220b76bb78023e9205b9d07f4abae
ERIC KRISHNA PERES BARBOSA <eric.krishna@gmail.com>
2019-02-05 23:29:22 -0200

    upload na_input function

--- R/na.R
+++ R/na.R
@@ -29,3 +29,72 @@ na_prop <- function(x, min_prop = 0) {
           NA_FREQ = V1 / N_TOTAL)] %>% 
     .[NA_FREQ >= min_prop]
 }
+
+
+#' NA imputation
+#'
+#' @description Wraps and extends \code{zoo} package's NA imputation functions. 
+#'
+#' @param x Vector to input data.
+#' @param how Inputation type. One of 'mean', 'median', 
+#' 'locf' (Last Observation Carried Backwards), 
+#' 'nocb' (Next Observation Carried Forward), 
+#' 'lin_interp' (linear interpolation) or 
+#' 'cub_spline' (cubic spline)
+#' @param window Window size. Used when \code{how} equals 'mean' or 'median'.
+#' @param ... Further arguments to zoo functions na.locf, na.approx or na.spline.
+#' 
+#' @importFrom zoo na.approx, na.spline, na.locf 
+#' @importFrom stringr str_split_fixed
+#' @export
+
+na_input <- function(x, how = 'median', window = Inf, ...) {
+  
+  if (!is.numeric(x)) 
+    stop("\nx must be numeric")
+  
+  if (! window > 0) 
+    stop("\nwindow must be a positive integer or +Inf")
+  
+  if (! how %in% c('mean','median','locf','nocb','lin_interp','cub_spline')) 
+    stop("\nhow must be one of c('mean','median','locf','nocb','lin_interp','cub_spline')")
+  # Include alignment option for how = 'mean' or 'median'. 
+  # Something like 'mean:center'. To split 'how', str_split_fixed(how, "[a-z]*(?=:)", n = 2)
+  
+  
+  if (how %in% c('mean','median')) {
+    
+    na_id <- which(is.na(x))
+    input <- tryCatch(
+      map_dbl(na_id, 
+              ~ {
+                inf_lim <- max((.x - window), 1L)
+                sup_lim <- min((.x + window), length(x))
+                x[inf_lim:sup_lim] %>% {.[!is.na(.)]} %>% {get(how)(.)}
+              }),
+      error = function(e) stop("\nTry increasing 'window' or changing 'how'")
+    )
+    x[na_id] <- input
+    x
+    
+  } else if (how == 'locf') {
+    
+    na.locf(x, na.rm = FALSE, ...)
+    
+  } else if (how == 'nocb') {
+    
+    na.locf(x, na.rm = FALSE, fromLast = TRUE, ...)
+    
+  } else if (how == 'lin_interp') {
+    
+    na.approx(x, na.rm = FALSE, ...)
+    
+  } else if (how == 'cub_spline') {
+    
+    na.spline(x, na.rm = FALSE, ...)
+    
+  }
+  
+}
+
+
